cb1e6ab3c60f2648fc88c9e8eac7fb31ac2f53d5
jckpt <krawcu333@gmail.com>
2023-04-05 14:58:40 +0200

    delete old way of fetching names

--- utils/getIcons.js
+++ utils/getIcons.js
@@ -1,36 +1,4 @@
-import { cache } from "react";
 import { BASE_URL } from "./baseURL";
-export const getChampionName = cache(async (championId) => {
-  const query = `query ($filter:String!){
-    championQuery: queryChampionsV1Contents(filter:$filter){
-      flatData{
-      slug
-      }
-    }
-  }`;
-  const response = await fetch(
-    "https://app.mobalytics.gg/api/league/gql/static/v1",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query,
-        variables: {
-          filter: `data/riotId/iv eq ${championId}`,
-        },
-      }),
-    }
-  );
-  if (!response.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  const result = await response.json();
-  const championName = result.data.championQuery[0].flatData.slug;
-  return championName;
-});
 export const getChampionIcon = async (championId) => {
   const response = await fetch(`${BASE_URL}/api/champion/${championId}`);
   const data = await response.json();
